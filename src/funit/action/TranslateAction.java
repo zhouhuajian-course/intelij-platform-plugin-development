d17c8cc96751a06d1da5f43c40fa657c1f2051de
zhouhuajian <zhouhuajian@company.com>
2022-11-02 18:30:07 +0800

    实现获取选中的文本

--- src/funit/action/TranslateAction.java
+++ src/funit/action/TranslateAction.java
@@ -2,12 +2,19 @@ package funit.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
 
 public class TranslateAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
-        System.out.println("just a test");
+        // System.out.println("just a test");
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        SelectionModel selectionModel = editor.getSelectionModel();
+        String selectedText = selectionModel.getSelectedText();
+        // System.out.println(selectedText);
     }
 }
